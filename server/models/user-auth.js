e216ef71b72e23641b2cffa84806c81a9dbace9d
ShahZaibKhan00 <shahzaib78903@gmail.com>
2024-10-10 03:48:15 +0500

    password hashed

--- server/models/user-auth.js
+++ server/models/user-auth.js
@@ -1,29 +1,47 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
 
 const userSchema = new mongoose.Schema({
             // column name Brackets me us ki properties jesy k string number ya 
                 //boolean ya kuch bhi jesa laravel me hota ha column k name k bad
     username: {             
         type: String,
-        require: true,
+        required: true,
     },
     email: {
         type: String,
-        require: true
+        required: true
     },
     phone: {
         type: String,
-        require: true
+        required: true
     },
     password: {
         type: String,
-        require: true
+        required: true
     },
     isAdmin: {
         type: Boolean,
         default: false
     }
 });
+
+userSchema.pre("save", async function (next) {
+    const user = this
+
+    if (!user.isModified()) {
+        next();
+    }
+
+    try {
+        const salt_round = await bcrypt.genSalt(12);
+        const hash_password = await bcrypt.hash(user.password, salt_round)
+        user.password = hash_password;
+    } catch (error) {
+        next(error);
+    }
+
+})
 
 
 const User = new mongoose.model("User", userSchema);
